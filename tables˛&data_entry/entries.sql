b77d7427cec25b745e1c9ce1c293817c4c4cef5a
majakosmrlj <mk37033@student.uni-lj.si>
2025-05-27 11:09:49 +0200

    popravljanje zasnove baze (serial key)

--- tables˛&data_entry/entries.sql
+++ tables˛&data_entry/entries.sql
@@ -1,29 +1,32 @@
---začetni insert za vsako tabelo
+INSERT INTO faks(ime, univerza)
+VALUES ('Fakulteta za matematiko in fiziko', 'Univerza v Ljubljani');
 
-INSERT INTO zapisek (id_zapiska, stevilo_strani, vrsta_dokumenta, naslov, jezik, download_link, id_predmeta, id_uporabnika)
-values (3, 54, 'pdf','numerične metode 2- vaje 2024/25', 'slovenščina', '//', 1, 1);
+INSERT INTO uporabnik(role, uporabnisko_ime, geslo, id_faksa)
+VALUES ('user', 'j_novak', 'j_novak', 1);
 
-INSERT INTO predmet(id_predmeta, ime, izobrazevalni_program, letnik)
-values (1, 'Numerične metode 1', 'Finančna matematika- dodiplomski', 2)
+INSERT INTO predmet(ime, izobrazevalni_program, letnik)
+VALUES ('Numerične metode 1', 'Finančna matematika- dodiplomski', 2);
 
+INSERT INTO predmet(ime, izobrazevalni_program, letnik)
+VALUES ('Numerične metode 2', 'Finančna matematika- dodiplomski', 2);
 
-INSERT INTO profesor(id_profesorja, ime, priimek)
-values(1, 'Ada',  'Šadl Praprotnik');
+INSERT INTO profesor(ime, priimek)
+VALUES ('Ada', 'Šadl Praprotnik');
 
-INSERT INTO faks(id_faksa, ime, univerza)
-values(1, 'Fakulteta za matematiko in fiziko', 'Univerza v Ljubljani');
+INSERT INTO zapisek (stevilo_strani, vrsta_dokumenta, naslov, jezik, download_link, id_predmeta, id_uporabnika)
+VALUES (54, 'pdf', 'numerične metode 2- vaje 2024/25', 'slovenščina', '//', 1, 1);
 
-INSERT INTO komentar(id_komentarja, vsebina, id_zapiska, id_uporabnika)
-values(1, 'kvalitetni zapiski, malo napak', 2, 1)
+INSERT INTO zapisek (stevilo_strani, vrsta_dokumenta, naslov, jezik, download_link, id_predmeta, id_uporabnika)
+VALUES (56, 'pdf', 'numerične metode 1- predavanja 2024/25', 'slovenščina', '//', 1, 1);
 
-INSERT INTO uporabnik(id_uporabnika, role, uporabnisko_ime, geslo, id_faksa)
-VALUES (1, 'user', 'j_novak', 'j_novak', 1);
+INSERT INTO komentar(vsebina, id_zapiska, id_uporabnika)
+VALUES ('kvalitetni zapiski, malo napak', 2, 1);
 
 INSERT INTO predmet_faks(id_predmeta, id_faksa)
-VALUES(1, 1)
+VALUES (1, 1);
 
 INSERT INTO profesor_faks(id_profesorja, id_faksa)
-VALUES(1, 1)
+VALUES (1, 1);
 
 INSERT INTO profesor_predmet(id_profesorja, id_predmeta)
-VALUES(1, 1)
+VALUES (1, 1);
--- tables˛&data_entry/generiranje_tabel.sql
+++ tables˛&data_entry/generiranje_tabel.sql
@@ -1,57 +1,13 @@
-CREATE TABLE zapisek (
-  id_zapiska INTEGER PRIMARY KEY,
-  stevilo_strani INTEGER,
-  vrsta_dokumenta TEXT,
-  naslov TEXT NOT NULL,
-  datum_objave DATE DEFAULT CURRENT_DATE,
-  jezik	TEXT NOT NULL,
-  download_link	TEXT NOT NULL,
-  id_predmeta INTEGER,
-  id_uporabnika INTEGER,
-  FOREIGN KEY (id_uporabnika) REFERENCES uporabnik(id_uporabnika),
-  FOREIGN KEY (id_predmeta) REFERENCES predmet(id_predmeta)
-);
-
-
-CREATE TABLE predmet (
-  id_predmeta INTEGER PRIMARY KEY,
-  ime TEXT NOT NULL,
-  izobrazevalni_program TEXT,
-  letnik INTEGER
-);
-
-
-CREATE TABLE profesor (
-  id_profesorja INTEGER PRIMARY KEY,
-  ime TEXT NOT NULL,
-  priimek TEXT NOT NULL
-);
-
-
-
+-- Tabela faks
 CREATE TABLE faks (
-  id_faksa INTEGER PRIMARY KEY,
+  id_faksa SERIAL PRIMARY KEY,
   ime TEXT NOT NULL,
   univerza TEXT 
 );
 
-
-CREATE TABLE komentar (
-  id_komentarja INTEGER PRIMARY KEY,
-  vsebina TEXT NOT NULL,
-  datum_objave TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  id_zapiska INTEGER,
-  id_uporabnika INTEGER,
-  FOREIGN KEY (id_uporabnika) REFERENCES uporabnik(id_uporabnika),
-  FOREIGN KEY (id_zapiska) REFERENCES zapisek(id_zapiska)
-);
-
-ALTER TABLE komentar
-ADD COLUMN id_nadkomentarja INTEGER REFERENCES komentar(id_komentarja);
-
-
+-- Tabela uporabnik
 CREATE TABLE uporabnik (
-  id_uporabnika INTEGER PRIMARY KEY,
+  id_uporabnika SERIAL PRIMARY KEY,
   role TEXT NOT NULL,
   uporabnisko_ime TEXT NOT NULL,
   geslo TEXT NOT NULL,
@@ -60,39 +16,83 @@ CREATE TABLE uporabnik (
   CONSTRAINT check_role CHECK (role IN ('admin','user'))
 );
 
+-- Tabela predmet
+CREATE TABLE predmet (
+  id_predmeta SERIAL PRIMARY KEY,
+  ime TEXT NOT NULL,
+  izobrazevalni_program TEXT,
+  letnik INTEGER
+);
 
+-- Tabela profesor
+CREATE TABLE profesor (
+  id_profesorja SERIAL PRIMARY KEY,
+  ime TEXT NOT NULL,
+  priimek TEXT NOT NULL
+);
+
+-- Tabela zapisek
+CREATE TABLE zapisek (
+  id_zapiska SERIAL PRIMARY KEY,
+  stevilo_strani INTEGER,
+  vrsta_dokumenta TEXT,
+  naslov TEXT NOT NULL,
+  datum_objave DATE DEFAULT CURRENT_DATE,
+  jezik TEXT NOT NULL,
+  download_link TEXT NOT NULL,
+  id_predmeta INTEGER,
+  id_uporabnika INTEGER,
+  FOREIGN KEY (id_uporabnika) REFERENCES uporabnik(id_uporabnika),
+  FOREIGN KEY (id_predmeta) REFERENCES predmet(id_predmeta)
+);
+
+-- Tabela komentar
+CREATE TABLE komentar (
+  id_komentarja SERIAL PRIMARY KEY,
+  vsebina TEXT NOT NULL,
+  datum_objave TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  id_zapiska INTEGER,
+  id_uporabnika INTEGER,
+  id_nadkomentarja INTEGER REFERENCES komentar(id_komentarja),
+  FOREIGN KEY (id_uporabnika) REFERENCES uporabnik(id_uporabnika),
+  FOREIGN KEY (id_zapiska) REFERENCES zapisek(id_zapiska)
+);
+
+
+-- Povezovalne tabele (relacijske)
+
+-- Tabela prenosi
 CREATE TABLE prenosi (
-    id_uporabnika INT,
-    id_zapiska INT,
-    PRIMARY KEY (id_uporabnika, id_zapiska),
-    FOREIGN KEY (id_uporabnika) REFERENCES uporabnik(id_uporabnika),
-    FOREIGN KEY (id_zapiska) REFERENCES zapisek(id_zapiska)
+  id_uporabnika INT,
+  id_zapiska INT,
+  PRIMARY KEY (id_uporabnika, id_zapiska),
+  FOREIGN KEY (id_uporabnika) REFERENCES uporabnik(id_uporabnika),
+  FOREIGN KEY (id_zapiska) REFERENCES zapisek(id_zapiska)
 );
 
+-- Tabela predmet_faks
 CREATE TABLE predmet_faks (
-    id_predmeta INT,
-    id_faksa INT,
-    PRIMARY KEY (id_predmeta, id_faksa),
-    FOREIGN KEY (id_predmeta) REFERENCES predmet(id_predmeta),
-    FOREIGN KEY (id_faksa) REFERENCES faks(id_faksa)
+  id_predmeta INT,
+  id_faksa INT,
+  PRIMARY KEY (id_predmeta, id_faksa),
+  FOREIGN KEY (id_predmeta) REFERENCES predmet(id_predmeta),
+  FOREIGN KEY (id_faksa) REFERENCES faks(id_faksa)
 );
 
+-- Tabela profesor_faks
 CREATE TABLE profesor_faks (
-    id_profesorja INT,
-    id_faksa INT,
-    PRIMARY KEY (id_profesorja, id_faksa),
-    FOREIGN KEY (id_profesorja) REFERENCES profesor(id_profesorja),
-    FOREIGN KEY (id_faksa) REFERENCES faks(id_faksa)
+  id_profesorja INT,
+  id_faksa INT,
+  PRIMARY KEY (id_profesorja, id_faksa),
+  FOREIGN KEY (id_profesorja) REFERENCES profesor(id_profesorja),
+  FOREIGN KEY (id_faksa) REFERENCES faks(id_faksa)
 );
 
-
-
+-- Tabela profesor_predmet
 CREATE TABLE profesor_predmet (
-    id_profesorja INT,
-    id_predmeta INT,
-    PRIMARY KEY (id_profesorja, id_predmeta),
-    FOREIGN KEY (id_profesorja) REFERENCES profesor(id_profesorja),
-    FOREIGN KEY (id_predmeta) REFERENCES predmet(id_predmeta)
+  id_profesorja INT,
+  id_predmeta INT,
+  PRIMARY KEY (id_profesorja, id_predmeta),
+  FOREIGN KEY (id_profesorja) REFERENCES profesor(id_profesorja),
+  FOREIGN KEY (id_predmeta) REFERENCES predmet(id_predmeta)
 );
-
-
